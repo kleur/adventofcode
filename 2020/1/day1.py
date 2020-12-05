45abe1d131ab73da49b84ab5fc4c0f0dbf5dfd07
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-05 16:50:02 +0100

    2020-day1: first working version with loops

--- 2020/1/day1.py
+++ 2020/1/day1.py
@@ -1,11 +1,29 @@
 # Find the two entries that sum to 2020; what do you get if you multiply them together?
+#
+# 1721 *
+# 979
+# 366
+# 299 *
+# 675
+# 1456
+#
+# In this list, the two entries that sum to 2020 are 1721 and 299.
+# Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
 
 def get_input(file):
     with open(file, 'r') as f:
-        lines = [line.strip() for line in f]
+        lines = [int(line.strip()) for line in f]
     return lines
 
 
-for line in get_input('test_input.txt'):
-    print(line)
+def find_numbers(expenses, total):
+    for num, expense in enumerate(expenses, start=1):
+        part = total - expense
+        for i in range(num, len(expenses)):
+            actual = expenses[i]
+            if actual == part:
+                return expense * part
+
+expense_report = get_input('input.txt')
+print("the answer:", find_numbers(expense_report, 2020))
