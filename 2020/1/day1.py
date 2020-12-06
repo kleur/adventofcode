f3be77ffe630f7e3de05e5c3a6afa34848d2aa12
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-06 21:34:05 +0100

    2020-day1: add recursive way

--- 2020/1/day1.py
+++ 2020/1/day1.py
@@ -11,19 +11,40 @@
 # Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
 
+# Get data from file
 def get_input(file):
     with open(file, 'r') as f:
         lines = [int(line.strip()) for line in f]
     return lines
 
 
-def find_numbers(expenses, total):
+def find_numbers_loops(expenses, total):
     for num, expense in enumerate(expenses, start=1):
         part = total - expense
         for i in range(num, len(expenses)):
             actual = expenses[i]
             if actual == part:
+                print("found", expense, "and", part, "product", expense * part)
                 return expense * part
 
-expense_report = get_input('input.txt')
-print("the answer:", find_numbers(expense_report, 2020))
+
+def find_numbers_recursive(array, n):
+    if n > 0:
+        first = array[0]
+        last = array[n - 1]
+        target = 2020 - first
+        # print("*", array, "first", first, "last", last, "target", target)
+        if last == target:
+            print("found", first, "and", last, "product", first * last)
+        if last > target:
+            find_numbers_recursive(array[1:], n - 1)
+        if last < target:
+            find_numbers_recursive(array, n - 1)
+
+
+# Driver program to test above function
+expenses = get_input('input.txt')
+expenses.sort(reverse=True)
+
+find_numbers_loops(expenses, 2020)
+find_numbers_recursive(expenses, len(expenses))
