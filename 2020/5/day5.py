dd49d3fee6c0b1f84e659e68e64d1609163b8856
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-09 09:39:15 +0100

    2020-day5: solve pt2 visually

--- 2020/5/day5.py
+++ 2020/5/day5.py
@@ -20,25 +20,36 @@ def determine_pos(str, s, e, x):
 
 
 def to_unique_ids(seat_ids):
-    uids = []
-    print()
+    uids = {}
     for seat_id in seat_ids:
         row_seq = seat_id[:7]
         col_seq = seat_id[7:]
         row = determine_pos(row_seq, 0, 128, 128)
         col = determine_pos(col_seq, 0, 8, 8)
         uid = row * 8 + col
-        uids.append(uid)
-        print([row, col], uid)
+        uids[row, col] = uid
         print()
     return uids
 
 
+def print_airplane_layout(seats):
+    for x in range(0, 128):
+        cols = []
+        for y in range(0, 8):
+            if (x, y) in seats:
+                seat = seats[(x, y)]
+                if seat < 100:
+                    cols.append(" " + str(seat))
+                else:
+                    cols.append(str(seat))
+            else:
+                cols.append("   ")
+        print(cols)
+
+
 boarding_passes = get_input("input.txt")
-unique_seat_id = to_unique_ids(boarding_passes)
-
-unique_seat_id.sort()
-
-print(unique_seat_id[-1])
-
-
+seats_dict = to_unique_ids(boarding_passes)
+unique_seat_ids = list(seats_dict.values())
+unique_seat_ids.sort()
+print("highest id", unique_seat_ids[-1])
+print_airplane_layout(seats_dict)
