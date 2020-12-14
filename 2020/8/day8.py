524daf9539457d1295f93de624679d3c23f12765
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-14 09:35:31 +0100

    2020-day8: fix pt2

--- 2020/8/day8.py
+++ 2020/8/day8.py
@@ -12,24 +12,56 @@ def to_operations(raw_data):
     return operations
 
 
-def run_rec(operations, n, acc):
+def run_rec1(operations, n, acc):
     if n not in operations:
         return acc
     else:
         cur = operations[n]
-        if cur["op"] == "nop":
-            pass
-        if cur["op"] == "acc":
-            acc += cur["arg"]
-        if cur["op"] == "jmp":
-            del operations[n]
-            return run_rec(operations, n + cur["arg"], acc)
         del operations[n]
-        return run_rec(operations, n + 1, acc)
+        if cur["op"] == "nop":
+            return run_rec1(operations, n + 1, acc)
+        if cur["op"] == "acc":
+            return run_rec1(operations, n + 1, acc + cur["arg"])
+        if cur["op"] == "jmp":
+            return run_rec1(operations, n + cur["arg"], acc)
+
+
+def run_rec2(operations, n, acc, executed):
+    if n == len(operations):
+        return [acc]
+    if n in executed:
+        return []
+    executed.add(n)
+    cur = operations[n]
+    if cur["op"] == "nop":
+        return run_rec2(operations, n + 1, acc, executed)
+    if cur["op"] == "acc":
+        return run_rec2(operations, n + 1, acc + cur["arg"], executed)
+    if cur["op"] == "jmp":
+        return run_rec2(operations, n + cur["arg"], acc, executed)
+
+
+def fix_program(opz):
+    final_result = 0
+    for n in range(0, len(opz)):
+        cur = opz[n]
+        if cur["op"] == "nop":
+            res2 = []
+            if cur["arg"] < 0 or cur["arg"] > 0:
+                ops_copy3 = opz.copy()
+                ops_copy3[n] = {"op": "jmp", "arg": cur["arg"]}
+                res2 = run_rec2(ops_copy3, 0, 0, set())
+            if len(res2) > 0:
+                final_result = res2[0]
+        if cur["op"] == "jmp":
+            ops_copy4 = opz.copy()
+            ops_copy4[n] = {"op": "nop", "arg": cur["arg"]}
+            res2 = run_rec2(ops_copy4, 0, 0, set())
+            if len(res2) > 0:
+                final_result = res2[0]
+    return final_result
 
 
 data = get_input("input.txt")
-ops = to_operations(data)
-
-result = run_rec(ops, 0, 0)
-print(result)
+print("accumulator at start of infinite loop", run_rec1(to_operations(data), 0, 0))
+print("accumulator at end of (fixed) program", fix_program(to_operations(data)))
