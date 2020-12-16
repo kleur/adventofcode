7942d80c2e88ac0b084c9aa0fd546e33844e125c
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-16 21:39:10 +0100

    2020-day9: solve pt2

--- 2020/9/day9.py
+++ 2020/9/day9.py
@@ -17,11 +17,27 @@ def find_numbers_recursive(slots, nums, target):
     return is_chosen or not_chosen
 
 
+def group_sum(nums, target, selection):
+    if len(nums) == 0:
+        return target == 0
+    if target == 0:
+        selection.sort()
+        if len(selection) > 1:
+            print(selection[0] + selection[-1])
+        return True
+    if target < 0:
+        return False
+    num = nums[0]
+    return group_sum(nums[1:], target - num, selection + [num])
+
+
 def find_first_number(nums, preamble, consider):
     for n in range(len(nums)):
         if n >= preamble:
             adds_up = find_numbers_recursive(2, nums[n - consider: n], nums[n])
             if not adds_up:
+                for x in range(len(nums)):
+                    group_sum(nums[x:], nums[n], [])
                 return nums[n]
 
 
