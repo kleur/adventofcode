bb55e0fb2a9d7be282ca22d2c0445f0b91224c30
Koen van de Leur <koen.van.de.leur@acorel.nl>
2020-12-22 09:44:04 +0100

    2020-day10: solve pt2 with test data

--- 2020/10/day10.py
+++ 2020/10/day10.py
@@ -14,14 +14,10 @@ def rec_connect(adapters, n, inp_rating, diffs):
     if n == len(adapters):
         diff = eff_max - inp_rating
         diffs[diff] += 1
-        # print("input rating:", inp_rating, "effectively:", eff_max, "adapter:", eff_max, "diff", diff)
-        print(diffs)
-        print("device rating", eff_max)
         return diffs[1] * diffs[3]
     cur = adapters[n]
     diff = cur - inp_rating
     diffs[diff] += 1
-    # print("input rating:", inp_rating, "effectively:", eff_max, "adapter:", cur, "diff", diff)
     return rec_connect(adapters, n + 1, cur, diffs)
 
 
@@ -30,6 +26,26 @@ def connect_adapters(adapters):
     return rec_connect(adapters, 0, 0, diffs)
 
 
-data = get_input("input.txt")
+def rec_find_paths(adapters, n, rating):
+    if n == len(adapters):
+        return 1
+    current_adapter = adapters[n]
+    diff_left = rating - current_adapter
+    if diff_left > 0:
+        do_choose = rec_find_paths(adapters, n + 1, current_adapter + 3)
+        not_choose = rec_find_paths(adapters, n + 1, rating)
+        return do_choose + not_choose
+    if diff_left == 0:
+        do_choose = rec_find_paths(adapters, n + 1, current_adapter + 3)
+        return do_choose
+    return 0
+
+
+def find_paths(adapters):
+    return rec_find_paths(adapters, 0, 3)
+
+
+data = get_input("test_input.txt")
 data.sort()
 print(connect_adapters(data))
+print(find_paths(data))
