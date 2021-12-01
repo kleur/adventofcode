556ea7cfa00cd1fecff0da4fbabbd1bc31c33d50
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-01 21:13:33 +0100

    2021-day1: refactor

--- 2021/1/day1.py
+++ 2021/1/day1.py
@@ -8,36 +8,23 @@ def get_input(file):
     return lines
 
 # recursion
-def part1_recursive(depths, count=0):
-    if len(depths) == 1:
+def recursive(depths, window, count=0):
+    if len(depths) == window:
         return count
-    if (depths[1] > depths[0]):
+    if (depths[window] > depths[0]):
         count += 1
-    return part1_recursive(depths[1:], count)
-    
-def part2_recursive(depths, count=0):
-    if len(depths) == 3:
-        return count
-    if (depths[3] > depths[0]):
-        count += 1
-    return part2_recursive(depths[1:], count)
+    return recursive(depths[1:], window, count)
 
 # iteration
-def part1_iteration(depths, count=0):
+def iteration(depths, window, count=0):
     for i in range(len(depths)):
-        if depths[i] > depths[i-1]:
-            count += 1
-    return count
-
-def part2_iteration(depths, count=0):
-    for i in range(len(depths)):
-        if depths[i] > depths[i-3]:
+        if depths[i] > depths[i-window]:
             count += 1
     return count
 
 # driver function
 input = get_input("test_input.txt")
-print("part 1 recursive:", part1_recursive(input))
-print("part 1 iterative:", part1_iteration(input))
-print("part 2 recursive:", part2_recursive(input))
-print("part 2 iterative:", part2_iteration(input))
+print("part 1 recursive:", recursive(input, 1))
+print("part 1 iterative:", iteration(input, 1))
+print("part 2 recursive:", recursive(input, 3))
+print("part 2 iterative:", iteration(input, 3))
