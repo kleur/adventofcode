c25e7798dffe926d7885fd9f97a77c85cf511591
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-01 20:53:53 +0100

    2021-day1: solve pt2

--- 2021/1/day1.py
+++ 2021/1/day1.py
@@ -14,6 +14,13 @@ def part1_recursive(depths, count=0):
     if (depths[1] > depths[0]):
         count += 1
     return part1_recursive(depths[1:], count)
+    
+def part2_recursive(depths, count=0):
+    if len(depths) == 3:
+        return count
+    if (depths[3] > depths[0]):
+        count += 1
+    return part2_recursive(depths[1:], count)
 
 # iteration
 def part1_iteration(depths, count=0):
@@ -22,7 +29,15 @@ def part1_iteration(depths, count=0):
             count += 1
     return count
 
+def part2_iteration(depths, count=0):
+    for i in range(len(depths)):
+        if depths[i] > depths[i-3]:
+            count += 1
+    return count
+
 # driver function
 input = get_input("test_input.txt")
 print("part 1 recursive:", part1_recursive(input))
 print("part 1 iterative:", part1_iteration(input))
+print("part 2 recursive:", part2_recursive(input))
+print("part 2 iterative:", part2_iteration(input))
