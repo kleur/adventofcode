fa4a8368852cffff795fe034387ff1498df314ae
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-02 12:48:07 +0100

    2021-day2: solve pt2 iterative & refactor

--- 2021/2/day2.py
+++ 2021/2/day2.py
@@ -15,14 +15,12 @@ def part1_recursive(commands, x=0, y=0):
     direction = split[0]
     incr = int(split[1])
     if direction == "down":
-        print("add", incr, "to y position, total", y + incr)
-        return part1_recursive(commands[1:], x, y + incr)
+        y += incr
     if direction == "up":
-        print("subtract", incr, "from y position, total", y - incr)
-        return part1_recursive(commands[1:], x, y - incr)
+        y -= incr
     if direction == "forward":
-        print("add", incr, "to x position, total", x + incr)
-        return part1_recursive(commands[1:], x + incr, y)
+        x += incr
+    return part1_recursive(commands[1:], x, y)
         
 def part2_recursive(commands, x=0, y=0, aim=0):
     if len(commands) == 0:
@@ -36,8 +34,7 @@ def part2_recursive(commands, x=0, y=0, aim=0):
         aim -= incr
     if direction == "forward":
         x += incr
-        extra = incr * aim
-        y += extra
+        y += incr * aim
     return part2_recursive(commands[1:], x, y, aim)
 
 # iteration
@@ -46,16 +43,26 @@ def part1_iteration(commands, x=0, y=0):
         split = commands[i].split()
         direction = split[0]
         incr = int(split[1])
-        print([direction, incr])
         if direction == "down":
-            print("add", incr, "to y position, total", y + incr)
             y += incr
         if direction == "up":
-            print("subtract", incr, "from y position, total", y - incr)
             y -= incr
         if direction == "forward":
-            print("add", incr, "to x position, total", x + incr)
             x += incr
+    return x * y
+    
+def part2_iteration(commands, x=0, y=0, aim=0):
+    for i in range(len(commands)):
+        split = commands[i].split()
+        direction = split[0]
+        incr = int(split[1])
+        if direction == "down":
+            aim += incr
+        if direction == "up":
+            aim -= incr
+        if direction == "forward":
+            x += incr
+            y += incr * aim
     return x * y
 
 # driver function
@@ -63,3 +70,4 @@ input = get_input("test_input.txt")
 print("part 1 recursive:", part1_recursive(input))
 print("part 1 iterative:", part1_iteration(input))
 print("part 2 recursive:", part2_recursive(input))
+print("part 2 iterative:", part2_iteration(input))
