d0b2c66d98456d6967eb08430e78784d0eaf4760
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-02 12:41:55 +0100

    2021-day2: solve pt2

--- 2021/2/day2.py
+++ 2021/2/day2.py
@@ -23,6 +23,22 @@ def part1_recursive(commands, x=0, y=0):
     if direction == "forward":
         print("add", incr, "to x position, total", x + incr)
         return part1_recursive(commands[1:], x + incr, y)
+        
+def part2_recursive(commands, x=0, y=0, aim=0):
+    if len(commands) == 0:
+        return x * y
+    split = commands[0].split()
+    direction = split[0]
+    incr = int(split[1])
+    if direction == "down":
+        aim += incr
+    if direction == "up":
+        aim -= incr
+    if direction == "forward":
+        x += incr
+        extra = incr * aim
+        y += extra
+    return part2_recursive(commands[1:], x, y, aim)
 
 # iteration
 def part1_iteration(commands, x=0, y=0):
@@ -46,3 +62,4 @@ def part1_iteration(commands, x=0, y=0):
 input = get_input("test_input.txt")
 print("part 1 recursive:", part1_recursive(input))
 print("part 1 iterative:", part1_iteration(input))
+print("part 2 recursive:", part2_recursive(input))
