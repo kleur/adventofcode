e9127f5c2f3cb51bb0e4d7d45e94c775aeecbe97
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-07 21:24:04 +0100

    2021-day5: solve pt2

--- 2021/5/day5.py
+++ 2021/5/day5.py
@@ -1,6 +1,3 @@
-import sys
-sys.setrecursionlimit(2500)
-
 # get the input
 def get_input(file):
     with open(file, 'r') as f:
@@ -10,17 +7,29 @@ def get_input(file):
 def to_position(raw, p):
     return [int(char) for char in raw.split()[p].split(",")]
     
-def to_positions(line1):
-    x1 = sorted([line1[0][0], line1[1][0]])
-    y1 = sorted([line1[0][1], line1[1][1]])
+def to_range(arr):
+    if len(arr) > 1 and arr[1] > arr[0]:
+        rng = [pos for pos in range(arr[0], arr[1]+1, 1)]
+    else:
+        rng = [pos for pos in range(arr[0], arr[1]-1, -1)]
+    return rng
     
-    x1range = [x for x in range(x1[0], x1[1]+1)]
-    y1range = [y for y in range(y1[0], y1[1]+1)]
+def to_positions(line1):    
+    x1range = to_range([line1[0][0], line1[1][0]])
+    y1range = to_range([line1[0][1], line1[1][1]])    
     
     positions = []
-    for x in x1range:
-        for y in y1range:
-            positions.append([x,y])
+    if len(x1range) > 1:
+        for i in range(len(x1range)):        
+            ix = min(i, len(x1range)-1)
+            iy = min(i, len(y1range)-1)
+            positions.append([x1range[ix], y1range[iy]])
+    else:
+        for i in range(len(y1range)):        
+            ix = min(i, len(x1range)-1)
+            iy = min(i, len(y1range)-1)
+            positions.append([x1range[ix], y1range[iy]])
+    
     return positions
     
 def intersect(line1, line2):
@@ -32,14 +41,11 @@ def intersect(line1, line2):
             if p1 == p2:
                 collisions.append(p1)
     return collisions
-    
 
 def part1_iteration(data):
     all_lines = map(lambda g : [to_position(g,0), to_position(g,2)], data)
     lines = filter(lambda u : u[0][0]==u[1][0] or u[0][1]==u[1][1], all_lines)
-    
     collisions = []
-    
     for i in range(len(lines)-1):
         cur = lines[i]
         print("checking with line", cur)
@@ -47,12 +53,24 @@ def part1_iteration(data):
             col_cur = intersect(cur, line)
             if len(col_cur) > 0:
                 collisions.extend(str(pos) for pos in col_cur)
-            # print("compare", line, "with", cur, "intersect", col_cur)
-        # print("")
         
+    return len(set(collisions))
+    
+def part2_iteration(data):
+    lines = map(lambda g : [to_position(g,0), to_position(g,2)], data)
+    collisions = []    
+    for i in range(len(lines)-1):
+        cur = lines[i]
+        print("checking with line", cur)
+        for line in lines[i+1:]:
+            col_cur = intersect(cur, line)
+            if len(col_cur) > 0:
+                collisions.extend(str(pos) for pos in col_cur)
+
     return len(set(collisions))
 
 # driver function
-input = get_input("input.txt")
+input = get_input("test_input.txt")
 print("part 1 iterative:", part1_iteration(input))
+print("part 2 iterative:", part2_iteration(input))
 
