b99502e1a57170762c9891047dc29cd6d607ed32
Koen van de Leur <koen.van.de.leur@acorel.nl>
2021-12-11 22:10:49 +0100

    2021-day7: solve pt2

--- 2021/7/day7.py
+++ 2021/7/day7.py
@@ -47,6 +47,33 @@ def align_recursive(grid, fuel=0):
         del grid[right]
     
     return align_recursive(grid, fuel)
+    
+def align_recursive_exponential(grid, fuel=0):
+    if len(grid.keys()) == 1:
+        return [grid.keys()[0], fuel]
+    
+    left = min(grid.keys())
+    right= max(grid.keys())
+    
+    left_crabs = [c+1 for c in grid[left]]
+    right_crabs = [c+1 for c in grid[right]]
+    
+    left_fuel = sum(left_crabs)
+    right_fuel = sum(right_crabs)
+    
+    if left_fuel <= right_fuel:
+        fuel += left_fuel
+        grid.pop(left)
+        next_left = grid[left+1]
+        grid[left+1] = (next_left + left_crabs)
+    
+    if right_fuel <= left_fuel:
+        fuel += right_fuel
+        grid.pop(right)
+        next_right = grid[right-1]
+        grid[right-1] = (next_right + right_crabs)
+    
+    return align_recursive_exponential(grid, fuel)
 
 # driver function
 def part1_recursive(crabs):
@@ -57,5 +84,13 @@ def part1_recursive(crabs):
     return align_recursive(crabs_dict)
     
 # driver function
+def part2_recursive(crabs):
+    crabs_dict = dict.fromkeys(range(min(crabs),max(crabs)), []) # create empty dict
+    arrays = dict((crab,[0 for i in range(crabs.count(crab))]) for crab in set(crabs)) # three crabs with x=2 -> [0,0,0] at key 2
+    crabs_dict.update(arrays)
+    return align_recursive_exponential(crabs_dict)
+    
+# driver function
 input = get_input("test_input.txt")
 print("part 1 recursive:", part1_recursive(input))
+print("part 2 recursive:", part2_recursive(input))
