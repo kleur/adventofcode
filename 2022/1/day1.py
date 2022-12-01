5485b0ae289d8ce0d422d863a7f515a077f91423
Koen van de Leur <koen.van.de.leur@acorel.nl>
2022-12-01 18:23:22 +0100

    2022-day1: solve pt2

--- 2022/1/day1.py
+++ 2022/1/day1.py
@@ -4,23 +4,29 @@ def get_input(file):
         lines = [line.strip() for line in f]
     return lines
 
-def part1_iterative(lines):
+def to_packages(lines):
     packages = []
     current_package = 0
     for i in range(0, len(lines)+1):
-        print(i, "of", len(lines))
         # make sure to catch the last one
         if (i == len(lines) or lines[i] == ""):
-            print("adding", current_package)
             packages.append(current_package)
             current_package = 0
         else:
-            print(lines[i])
             current_package += int(lines[i])
-    print(packages)
+    return packages
+
+def part1_iterative(lines):
+    packages = to_packages(lines)
     return max(packages)
+
+def part2_iterative(lines):
+    packages = to_packages(lines)
+    packages.sort()
+    return sum(packages[-3:])
 
 # driver function
 input = get_input("test_input.txt")
 
 print("part 1 iterative:", part1_iterative(input))
+print("part 2 iterative:", part2_iterative(input))
