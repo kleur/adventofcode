ea7a8735865e1333a5faee842193854df5ccd7d9
Koen van de Leur <koen.van.de.leur@acorel.nl>
2022-12-03 08:42:54 +0100

    2022-day3: solve pt2

--- 2022/3/day3.py
+++ 2022/3/day3.py
@@ -17,9 +17,19 @@ def part1_iterative(lines):
         common = list(set(rucksack[0]).intersection(set(rucksack[1])))[0]
         priorities.append(prio_mapping.index(common))
     return sum(priorities)
+    
+def part2_recursive(rucksacks, group, prios):
+    if len(rucksacks) == 0:
+        return prios
+    group.append(rucksacks[0])
+    if (len(group) == 3):
+        common = list(set.intersection(*[set(rucksack) for rucksack in group]))[0]
+        prios += prio_mapping.index(common)
+        group = [] # reset the group
+    return part2_recursive(rucksacks[1:], group, prios)
 
 # driver function
 input = get_input("test_input.txt")
 
 print("part 1 iterative:", part1_iterative(input))
-#print("part 2 iterative:", part2_iterative(input))
+print("part 2 recursive:", part2_recursive(input, [], 0))
