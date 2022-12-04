3c8937dd710fd81e429c2dbdd221afe380516ede
Koen van de Leur <koen.van.de.leur@acorel.nl>
2022-12-04 09:28:30 +0100

    2022-day4: solve pt2

--- 2022/4/day4.py
+++ 2022/4/day4.py
@@ -23,7 +23,24 @@ def part1_recursive(lines, acc):
     if (l_contains or r_contains): acc += 1
     return part1_recursive(lines[1:], acc)
 
+def part2_recursive(lines, acc):
+    if len(lines) == 0:
+        return acc
+    pair = [[int(b) for b in a.split("-")] for a in lines[0].split(",")]
+    
+    start1 = pair[0][0]
+    start2 = pair[1][0]
+    end1 = pair[0][1]
+    end2 = pair[1][1]
+    
+    hit1 = start1 <= start2 <= end1 or start1 <= end2 <= end1
+    hit2 = start2 <= start1 <= end2 or start2 <= end1 <= end2
+    if (hit1 or hit2): acc += 1
+
+    return part2_recursive(lines[1:], acc)
+
 # driver function
 input = get_input("test_input.txt")
 
 print("part 1 recursive:", part1_recursive(input, 0))
+print("part 2 recursive:", part2_recursive(input, 0))
