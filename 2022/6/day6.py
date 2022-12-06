9298e837187d7228162466051c90ce9f01c8c486
Koen van de Leur <koen.van.de.leur@acorel.nl>
2022-12-06 09:17:50 +0100

    2022-day6: solve pt2

--- 2022/6/day6.py
+++ 2022/6/day6.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(2000)
+sys.setrecursionlimit(10000)
 
 # get the input
 def get_input(file):
@@ -15,14 +15,15 @@ def find_start(signal, i, marker_length):
         return (i+marker_length)
     return find_start(signal, i+1, marker_length)
         
-def part1_recursive(signals):
+def part1_recursive(signals, marker_length):
     indices = []
     for signal in signals:
-        indices.append(find_start(signal, 0, 4))
+        indices.append(find_start(signal, 0, marker_length))
     return indices
     
 
 # driver function
 input = get_input("test_input.txt")
 
-print("part 1 recursive:", part1_recursive(input))
+print("part 1 recursive:", part1_recursive(input, 4))
+print("part 2 recursive:", part1_recursive(input, 14))
